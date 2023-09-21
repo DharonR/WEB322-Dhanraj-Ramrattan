0f11afac1bcb520b3267f70cb764a11cbe6d1ffd
Dharon Ramrattan <dharon1234@gmail.com>
2023-09-20 20:30:30 -0400

    added initial code. runs but it still has errors

--- index.js
+++ index.js
@@ -1,2 +1,34 @@
+const EventEmitter = require("events");
 
-console.log("Hello World");
+const trafficLights = new EventEmitter();
+
+//Array and index for array to hold colors
+const lightColors = ['Red', 'Yellow', 'Green'];
+let currentLightIndex = 0; //default is set to the index for Red
+
+function changeLights() {
+    trafficLights.emit('changeColor', lightColors[currentLightIndex]);
+    console.log("Current Light: ", lightColors[currentLightIndex]);
+    currentLightIndex++;
+    if(currentLightIndex == 3){ currentLightIndex = 0;};
+}
+
+if (currentLightIndex < 3){
+    if (currentLightIndex == 0){
+        setInterval(changeLights, 5000);
+    }
+    else if (currentLightIndex == 1){
+        setInterval(changeLights, 2000);
+    }
+    else if (currentLightIndex == 2){
+        setInterval(changeLights, 5000);
+    }
+};
+
+// EventEmitter function
+trafficLights.on('changeColor', (color) => {
+    console.log('Current Light: ', color);
+})
+
+//Start
+console.log("Current Light: ", lightColors[currentLightIndex]);
