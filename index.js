0b5cc8d09fa27b2d85e6a6c9a44b2feb3db4f96d
Dharon Ramrattan <dharon1234@gmail.com>
2023-09-20 20:51:00 -0400

    debugging

--- index.js
+++ index.js
@@ -3,32 +3,40 @@ const EventEmitter = require("events");
 const trafficLights = new EventEmitter();
 
 //Array and index for array to hold colors
-const lightColors = ['Red', 'Yellow', 'Green'];
+const lightColors = ['Green', 'Yellow', 'Red'];
 let currentLightIndex = 0; //default is set to the index for Red
 
 function changeLights() {
     trafficLights.emit('changeColor', lightColors[currentLightIndex]);
     console.log("Current Light: ", lightColors[currentLightIndex]);
     currentLightIndex++;
-    if(currentLightIndex == 3){ currentLightIndex = 0;};
+    if(currentLightIndex == 3){ currentLightIndex = 0;}
 }
 
-if (currentLightIndex < 3){
-    if (currentLightIndex == 0){
-        setInterval(changeLights, 5000);
-    }
-    else if (currentLightIndex == 1){
-        setInterval(changeLights, 2000);
-    }
-    else if (currentLightIndex == 2){
-        setInterval(changeLights, 5000);
-    }
-};
+
+
+// setInterval(() => {
+//     if (currentLightIndex == 0 || currentLightIndex == 2){
+//         setInterval(changeLights, 5000);
+//     }
+//     else {
+//         setInterval(changeLights, 2000);
+//     }
+// }, 5000);
+
+// if (currentLightIndex < 3){
+//     if (currentLightIndex == 0 || currentLightIndex == 2){
+//         setInterval(changeLights, 5000);
+//     }
+//     else {
+//         setInterval(changeLights, 2000);
+//     }
+// };
 
 // EventEmitter function
 trafficLights.on('changeColor', (color) => {
-    console.log('Current Light: ', color);
+    //console.log('Current Light: ', color);
 })
 
 //Start
-console.log("Current Light: ", lightColors[currentLightIndex]);
+console.log("Current Light: ", lightColors[2]);
