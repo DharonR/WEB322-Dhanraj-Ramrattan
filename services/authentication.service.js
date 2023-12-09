a33e0262f279d0744dc175e82ed5c2bc9c070b2f
Dharon Ramrattan <dharon1234@gmail.com>
2023-12-08 22:29:05 -0500

    fixed authentication method with hardcoded values

--- services/authentication.service.js
+++ services/authentication.service.js
@@ -1,10 +1,17 @@
-const userData = require("../data/fakeUsers.json");
+const UsersService = require('../services/users.service'); // Adjust the path based on your actual file structure
 
 class AuthenticationService {
-  static authenticate(username, password) {
-    return userData.find(
-      (user) => user.email === username && user.password === password && user.isAdmin === true
-    );
+  static async authenticate(username, password) {
+    try {
+      if (username === 'admin' && password === 'password') {
+        return { username: 'admin', isAdmin: true };
+      }
+      const user = await UsersService.findAll({ email: username, password: password, isAdmin: true});
+      return user.length > 0 ? user[0] : null;
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
   }
 }
 
